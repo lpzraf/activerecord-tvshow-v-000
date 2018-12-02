c5a92a006832509fc4263a3975839feefefca35a
Rafael Lopez <lpz.raf@gmail.com>
2018-12-02 20:31:19 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,12 @@
 class Show < ActiveRecord::Base
+  
+  
+  def self.highest_rating
+  self.maximum('rating')
+  end
+  
+  def self.most_popular_show
+  self.where('rating = ?', self.highest_rating)[0]
+  end
+  
 end
